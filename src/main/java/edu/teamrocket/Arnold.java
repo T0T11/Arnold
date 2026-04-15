b582d1ac33a5df83b04dc5a20fb41b8b47ba09d4
T0T11 <tomas.orellano@gmail.com>
2026-04-15 21:23:35 +0200

    feat(Arnold): agregar la funcion de poner las dos variables

--- src/main/java/edu/teamrocket/Arnold.java
+++ src/main/java/edu/teamrocket/Arnold.java
@@ -2,50 +2,60 @@ package edu.teamrocket;
 import java.lang.Enum.EnumDesc;
 import edu.teamrocket.Planetas;
 
-public class Arnold {
-    private double peso ;
-    private static Arnold instance; /* tiene que el constructor ser privado*/
 
-    private Arnold(double peso) /* constructor privado*/ {
+
+public class Arnold {
+
+    private double peso;
+
+    public Arnold(double peso) {
         this.peso = peso;
     }
 
-    public static Arnold getInstance(double peso ){
-        if (instance == null) {
-            instance = new Arnold(peso);
+    public void Ejecucion(Planetas planetaElegido) {
+
+        System.out.printf("Your weight on %s is %.2f N%n",
+                planetaElegido.name(),
+                planetaElegido.pesoSuperficie(peso));
+
+        System.out.println("\nYour weight on all planets:");
+        for (Planetas p : Planetas.values()) {
+            System.out.printf("Your weight on %s is %.2f N%n",
+                    p.name(), p.pesoSuperficie(peso));
         }
-        return  instance; /*esto es el Singleton*/
+
+        System.out.println("\nYour weight only on the terrestrial planets:");
+        for (Planetas p : Planetas.getPlanetasTerrestres()) {
+            System.out.printf("Your weight on %s is %.2f N%n",
+                    p.name(), p.pesoSuperficie(peso));
+        }
+
+        System.out.println("\nYour weight on the ice giants:");
+        for (Planetas p : Planetas.getPlanetasHelados()) {
+            System.out.printf("Your weight on %s is %.2f N%n",
+                    p.name(), p.pesoSuperficie(peso));
+        }
+
+        System.out.println("\nYour weight on the gas giants:");
+        for (Planetas p : Planetas.getPlanetasGaseosos()) {
+            System.out.printf("Your weight on %s is %.2f N%n",
+                    p.name(), p.pesoSuperficie(peso));
+        }
     }
-
-    public double getPeso() {
-        return peso;
-    }
-
-
-    public void Ejecucion() {
-          for (Planetas planetas : Planetas.values() ) {
-              System.out.println("Your weight on %s is %f N%n",planetas.name(), planetas.pesoSuperficie(peso);}
-
-
-          System.out.println("\nYour weight only on the terrestrial planets: ");
-          for (Planetas planetas : Planetas.getPlanetasTerrestres()){ /* de la variable llamada planetas, sacada del Enum Planetas quiero que le apliques la funcion dicha de planetas */
-              System.out.println("Your weight on %s is %f N%n", planetas.name(), planetas.pesoSuperficie(peso);
-          }
-          System.out.println("\n“your weight on the ice giants:");
-          for (Planetas planetas : Planetas.getPlanetasHelados()) {
-              System.out.println("Your weight on %s is %f N%n", planetas.name(), planetas.pesoSuperficie(peso);
-          }
-
-          System.out.println("\nYour weight only on the gas giant planets:");
-          for (Planetas planetas : Planetas.getPlanetasGaseosos()){
-              System.out.println("Your weight on %s is %f N%n", planetas.name(), planetas.pesoSuperficie(peso);
-          }
-
-     }
 
     public static void main(String[] args) {
-        Arnold arnold = Arnold.getInstance(90); /*mi peso */
-        arnold.Ejecucion();
-    }
 
+        if (args.length < 2) {
+            System.out.println("Uso: java Arnold <peso_en_kg> <PLANETA>");
+            return;
+        }
+
+        double peso = Double.parseDouble(args[0]);
+        String planetaStr = args[1].toUpperCase(); /* esto se hace debido a que el enum de planetas estan en mayuscul y lo guardo en un string */
+
+        Planetas planeta = Planetas.valueOf(planetaStr);  /*hago una variable planeta que se guarda en el enum y guardo el valueof del string en mayusculas*/
+
+        Arnold arnold = new Arnold(peso);
+        arnold.Ejecucion(planeta);
+    }
 }
